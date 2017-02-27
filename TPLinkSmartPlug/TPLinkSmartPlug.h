d62dcfc923d200ef03b1ef8676c4848fb3752eee
Colin Harris <col.w.harris@gmail.com>
2017-02-27 21:11:36 +0800

    Initial Commit

---
+++ TPLinkSmartPlug/TPLinkSmartPlug.h
@@ -0,0 +1,19 @@
+//
+//  TPLinkSmartPlug.h
+//  TPLinkSmartPlug
+//
+//  Created by Colin Harris on 27/2/17.
+//  Copyright © 2017 Colin Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPLinkSmartPlug.
+FOUNDATION_EXPORT double TPLinkSmartPlugVersionNumber;
+
+//! Project version string for TPLinkSmartPlug.
+FOUNDATION_EXPORT const unsigned char TPLinkSmartPlugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPLinkSmartPlug/PublicHeader.h>
+
+
